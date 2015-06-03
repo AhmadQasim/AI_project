5796cbcba00f38585685bfd36a8f4d70907c15ce
Ahmad Bin Qasim <Ahmad.live.1@gmail.com>
2015-06-03 19:09:47 +0500

    Update image_classify.py

--- image_classify.py
+++ image_classify.py
@@ -1,85 +1,60 @@
-import cv2
-from numpy import zeros, histogram, hstack, vstack, savetxt
-import scipy.cluster.vq as vq
+from os.path import join
 import svmutil
 from cPickle import load
+from feature_detector import extractSift, computeHistograms,writeHistogramsToFile,get_categories,get_imgfiles
 
 HISTOGRAMS_FILE = 'testdata.svm'
-CODEBOOK_FILE = 'dataset_tinycodebook.file'
+CODEBOOK_FILE = 'datasetcodebook.file'
+DATASETPATH = 'D:\\AI_project-master\\dataset'
 MODEL_FILE = 'trainingdata.svm.model'
 CAT_LABEL = 'cat.txt'
-
-def extractSift(input_file):
-    print "extracting Sift features"
-    all_features_dict = {}
-    image = cv2.imread(input_file)
-    gray= cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    sift =cv2.SIFT()
-    kp, descriptors = sift.detectAndCompute(gray,None)
-    print "gathering sift features for", input_file,
-    print descriptors.shape
-    all_features_dict[input_file] = descriptors
-    return all_features_dict
-    
-def computeHistograms(codebook, descriptors):
-    code, dist = vq.vq(descriptors, codebook)
-    histogram_of_words, bin_edges = histogram(code,
-                                              bins=range(codebook.shape[0] + 1),
-                                              normed=True)
-    return histogram_of_words
-    
-def writeHistogramsToFile(nwords, labels, fnames, all_word_histgrams, features_fname):
-    data_rows = zeros(nwords + 1)  # +1 for the category label
-    histogram = all_word_histgrams
-    if (histogram.shape[0] != nwords):  # scipy deletes empty clusters
-        nwords = histogram.shape[0]
-        data_rows = zeros(nwords + 1)
-        print 'nclusters have been reduced to ' + str(nwords)
-    data_row = hstack((labels[fnames], histogram))
-    data_rows = vstack((data_rows, data_row))
-    data_rows = data_rows[1:]
-    fmt = '%i '
-    for i in range(nwords):
-        fmt = fmt + str(i) + ':%f '
-    savetxt(features_fname, data_rows, fmt)
+TEST_SAMPLES= 2
 
 print "---------------------"
 print "## extract Sift features"
 all_files = []
 all_files_labels = {}
 all_features = {}
+cat_label = {}
+cats = get_categories(DATASETPATH)
+ncats = len(cats)
 
-codebook_file = CODEBOOK_FILE
-model_file = MODEL_FILE
+for cat, label in zip(cats, range(ncats)):
+    cat_path = join(DATASETPATH, cat)
+    cat_files = get_imgfiles(cat_path, TEST_SAMPLES)
+    cat_features = extractSift(cat_files)
+    all_files = all_files + cat_files
+    all_features.update(cat_features)
+    cat_label[cat] = label
+    for i in cat_files:
+        all_files_labels[i] = label
+
 #fnames = filename = raw_input('Enter input file name: ')
-fname = 'scorpion.jpg'
-all_features = extractSift(fname)
-all_files_labels[fname] = 4  # label is unknown
 
 print "---------------------"
-print "## loading codebook from " + codebook_file
-with open(codebook_file, 'rb') as f:
+print "## loading codebook from " + CODEBOOK_FILE
+with open(CODEBOOK_FILE, 'rb') as f:
     codebook = load(f)
 
 print "---------------------"
 print "## computing visual word histograms"
-word_histgram = computeHistograms(codebook, all_features[fname])
+all_word_histgrams={}
+for imagefname in all_features:
+        word_histgram = computeHistograms(codebook, all_features[imagefname])
+        all_word_histgrams[imagefname] = word_histgram
 
 print "---------------------"
 print "## write the histograms to file to pass it to the svm"
 nclusters = codebook.shape[0]
 writeHistogramsToFile(nclusters,
                       all_files_labels,
-                      fname,
-                      word_histgram,
+                      all_files,
+                      all_word_histgrams,
                       HISTOGRAMS_FILE)
 
 print "---------------------"
 print "## test data with svm"
 
 y,x=svmutil.svm_read_problem(HISTOGRAMS_FILE)
-model=svmutil.svm_load_model(model_file)
+model=svmutil.svm_load_model(MODEL_FILE)
 result = svmutil.svm_predict(y,x,model)
-cat_label = load(open("cat.txt", "rb" ))
-print cat_label
-print result[0][0]
